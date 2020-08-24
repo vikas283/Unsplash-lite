d93da23b662239e16b4dbaa41a13630bc97e9641
Vikas <vikassin563@gmail.com>
2020-08-24 16:19:05 +0530

    configuration files added

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component{
+
+    render(){
+        return(
+            <div>
+            Hello From App.
+            </div>
+        );
+    }
+}
+
+export default App;
